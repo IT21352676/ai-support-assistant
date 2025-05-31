b5ecee8831ad985b7e150cde3eff9e4e09b8d0db
Hirusha Sasanka <100258430+IT21352676@users.noreply.github.com>
2025-06-01 02:24:36 +0530

    Mistral: Mixtral 8x7B model intergration

--- app/groq_service.py
+++ app/groq_service.py
@@ -1,6 +1,7 @@
 import httpx
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -13,14 +14,18 @@ async def generate_llm_response(prompt: str):
         "Content-Type": "application/json"
     }
 
-    payload = {
-        "model": "openai/gpt-3.5-turbo",
-        "messages": [
-            {"role": "user", "content": prompt}
-        ]
-    }
+    data=json.dumps({
+    "model": "mistralai/mixtral-8x7b-instruct",
+    "messages": [
+      {
+        "role": "user",
+        "content": prompt
+      }
+    ],
+    
+  })
 
     async with httpx.AsyncClient() as client:
-        response = await client.post("https://openrouter.ai/api/v1/chat/completions", json=payload, headers=headers)
+        response = await client.post("https://openrouter.ai/api/v1/chat/completions", data=data, headers=headers)
         response.raise_for_status()
         return response.json()["choices"][0]["message"]["content"]
