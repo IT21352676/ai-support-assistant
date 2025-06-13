e079138715fb307eeeb9c9cf89d13ab0c681c9bb
Hirusha Sasanka <100258430+IT21352676@users.noreply.github.com>
2025-06-13 13:53:41 +0530

    changes

--- app/cache_service.py
+++ app/cache_service.py
@@ -5,8 +5,8 @@ import json
 
 redis_client = redis.Redis(host="localhost", port=6379, db=0)
 
-def create_cache_key(customer_type: str, question: str) -> str:
-    key = f"{customer_type}:{question}"
+def create_cache_key(user_id:str,customer_type: str, question: str) -> str:
+    key = f"{user_id}:{customer_type}:{question}"
     return hashlib.sha256(key.encode()).hexdigest()
 
 def get_cached_response(key: str):
@@ -19,9 +19,10 @@ def add_to_user_history(user_id: str, question: str, answer: str):
     history_key = f"user_history:{user_id}"
     entry = json.dumps({"question": question, "answer": answer})
     redis_client.rpush(history_key, entry)
-    redis_client.ltrim(history_key, -5,-1)  # Keep only last 5 entries
+    redis_client.ltrim(history_key, -5,-1)  
 
 def get_user_history(user_id: str):
     history_key = f"user_history:{user_id}"
     history_data = redis_client.lrange(history_key, 0, -1)
     return [json.loads(h.decode()) for h in history_data]
+
